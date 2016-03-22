a7c4a976ca487e46d9dc13999f91623cff16380e
Tomasz Czaja <cza.tom84@gmail.com>
2016-03-22 13:25:51 +0100

    Pusty szablon ASP.NET MVC w wersji 5.0

---
+++ Konferencja/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Konferencja.Startup))]
+namespace Konferencja
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
